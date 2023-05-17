339acf8a62716dbc4841c0dc21f48255e9191630
Sameer Ahuja <sameer.ahuja@gmail.com>
2023-05-17 15:14:02 -0700

    Bump version to 0.3 and add console script entry point
    - Version number updated from 0.2 to 0.3 in setup.py
    - Added a console script entry point, allowing the user to run 'ai-commit-gen' from the command line to execute the script.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="ai-commit-gen",
-    version="0.2",
+    version="0.3",
     packages=find_packages(),
     scripts=["ai-commit.py"],
     install_requires=[
@@ -17,4 +17,9 @@ setup(
     description="A binary to generate commit messages or commits using generative models.",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    entry_points={
+        "console_scripts": [
+            "ai-commit-gen=ai-commit:main",  # This allows the user to run 'autocommit' from the command line to execute your script
+        ],
+    },
 )
