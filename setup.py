bff8a110dc0b46dc9a400d402152c41f4193a444
Sameer Ahuja <sameer.ahuja@gmail.com>
2023-05-17 15:25:01 -0700

    Update version in setup.py to 0.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="ai-commit-gen",
-    version="0.3",
+    version="0.4",
     packages=find_packages(),
     scripts=["ai_commit_gen.py"],
     install_requires=[
